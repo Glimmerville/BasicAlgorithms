69c52d41e15404f16292bce86fdc2f41d13d8b12
James M. Allen <james.m.allen@gmail.com>
2016-05-25 11:05:32 -0400

    Basic Linked List

---
+++ BasicAlgorithms.Test/LinkedListTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BasicAlgorithms.Test
+{
+    [TestFixture]
+    class LinkedListTest
+    {
+        [Test]
+        public void BasicTestsOfLinkedList()
+        {
+            LinkedList A = new LinkedList();
+            A.Add(3);
+            A.Add(7);
+            A.Add(7);
+            A.Add(8);
+            A.Add(12);
+            A.Add(9);
+            A.Add(2);
+
+            Assert.That(A.Length, Is.EqualTo(7));
+            Assert.That(A.GetItem(3), Is.EqualTo(8));
+        }
+    }
+}
---
+++ BasicAlgorithms/LinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAlgorithms
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class LinkedList
+    {
+        int length;
+        Node firstNode;
+        Node lastNode;
+
+        public LinkedList()
+        {
+            length = 0;
+            firstNode = null;
+            lastNode = null;
+        }
+
+        public int Length {
+            get
+                {
+                return length; 
+                }
+        }
+
+        public void Add(int v)
+        {
+            Node node = new Node(v);
+            if (lastNode != null)
+            {
+                lastNode.Next = node;
+            } 
+            else
+            {
+                firstNode = node;
+            }
+            lastNode = node;
+            length++;
+        }
+
+        public int GetItem(int index)
+        {
+            Node currentNode = firstNode;
+            for (int i = 0; i < index; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            return currentNode.Value;
+        }
+    }
+}
